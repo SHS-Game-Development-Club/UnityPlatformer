48e553f86a54d71d9346c1f1e482ab81397c4467
ochang23 <chao5915@lgsstudent.org>
2022-05-08 21:46:15 -0700

    Created a fall detector as well as checkpoints
    The checkpoints should be able to just be copy pasted as needed, and the fall detector moves below the player as they move

--- GDC_Platformer/Assets/PlayerMovement.cs
+++ GDC_Platformer/Assets/PlayerMovement.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
-{
+{ 
+    private Vector3 respawnPoint; //creates a respawn point
+    public GameObject fallDetector; 
+    
     private Rigidbody2D rb;
     // Start is called before the first frame update
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        respawnPoint = transform.position;
+        
+
     }
 
     // Update is called once per frame
@@ -19,6 +25,20 @@ public class PlayerMovement : MonoBehaviour
 
         if (Input.GetButtonDown("Jump")){
             rb.velocity = new Vector2(rb.velocity.x, 10f);
+            
+        }
+        fallDetector.transform.position = new Vector2(transform.position.x , fallDetector.transform.position.y);  
+    }
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag  == "FallDetector")
+        {
+            transform.position = respawnPoint;  //resets player's position to respawn point if they fall
+        }
+        else if (collision.tag == "Checkpoint")
+        {
+            respawnPoint = transform.position;
         }
     }
 }
